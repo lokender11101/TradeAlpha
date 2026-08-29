b29380ba92d3c4605c3d6e9252948012326231bd
lokender rao <lokenderrao@lokenders-MacBook-Air.local>
2026-08-29 23:40:59 +0530

    chore: remove scratch files

--- scratch/patch.sh
+++
@@ -1,7 +0,0 @@
-sed -i '' '/await tx.orderFill.update({/,/});/d' apps/api/src/services/order.service.ts
-sed -i '' '312a\
-        await tx.orderFill.update({\
-          where: { id: fill.id },\
-          data: { realizedPnl: fillRealizedPnl }\
-        });\
-' apps/api/src/services/order.service.ts
